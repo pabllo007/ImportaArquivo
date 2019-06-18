df89f014e21abe77497e084eabf0ba9740c80d16
= <=>
2019-06-18 09:25:10 -0300

    alteração de estrutura do projeto

--- src/main/java/br/com/storeFiles/repository/ImportaArquivoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.storeFiles.repository;
-
-import br.com.storeFiles.model.ImportacaoArquivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImportaArquivoRepository extends JpaRepository<ImportacaoArquivo, Long> {
-   // List<ImportacaoArquivo> findByIdArquivos(Long id);
-}
